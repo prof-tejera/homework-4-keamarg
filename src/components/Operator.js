842d27ce8ef742ff9c512e0e576ec9f976e2e2fe
Martin Gundtoft <marg@kea.dk>
2021-10-07 15:01:51 +0200

    first commit

--- src/components/Operator.js
+++ src/components/Operator.js
@@ -1,16 +1,35 @@
-import { Component } from 'react';
-import styled from 'styled-components';
+import { Component } from "react";
+import styled, { css } from "styled-components";
 
 const Container = styled.div`
+  /* Adapt the color based on the type prop */
+  ${(props) =>
+    props.type === "operator" &&
+    css`
+      background: #287ed4;
+      color: white;
+    `}
+  ${(props) =>
+    props.type === "other" &&
+    css`
+      background: #dfdfdf;
+    `}
+
   padding: 10px;
   border: 1px solid black;
-  width: 60px;
+  width: 53px;
+  text-align: center;
+  line-height: 40px;
+  cursor: pointer;
 `;
 
 class Operator extends Component {
   render() {
     return (
-      <Container onClick={() => this.props.onClick(this.props.value)}>
+      <Container
+        type={this.props.type}
+        onClick={() => this.props.onClick(this.props.value)}
+      >
         {this.props.value}
       </Container>
     );
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,18 @@
-import { Component } from 'react';
-import styled from 'styled-components';
+import { Component } from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   border: 1px solid black;
-  width: 300px;
-  height: 70px;
+  width: 250px;
+  height: 30px;
   text-align: right;
   margin-bottom: 10px;
+  background-color: black;
+  color: white;
+  padding: 20px 30px 20px 20px;
+  line-height: 30px;
+  font-size: 40px;
+  overflow: hidden;
 `;
 
 class Screen extends Component {
